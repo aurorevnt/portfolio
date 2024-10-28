8612f2ef38ece78f472414300208594cb2f5627d
Aurore VERNET <aurore.vernet@live.com>
2024-10-28 16:56:40 +0100

    working on about page

--- vue/About.js
+++ vue/About.js
@@ -2,7 +2,33 @@ export default {
     template:
     `
     <section id="home">
-        <div>prout</div>
+        <h2 class="white-box">Qui suis-je ?</h2>
+        <article>
+            <div>
+                <h4>AURORE VERNET</h4>
+                <div>
+                    <img src="">
+                    <div>
+                        <img src="">
+                        <span>Grenoble & périphéries / Lyon / Amiens</span>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <ul>
+                    <li>Étudiante en <span class="bold">développement web</span></li>
+                    <li>En formation à <span class="bold">La Manu</span>, à Amiens</li>
+                    <li>25 ans</li>
+                </ul>
+                <div>
+                    <img src="">
+                    <img src="" class="icon">     
+                    <img src="" class="icon">     
+                    <img src="" class="icon">     
+                    <img src="" class="icon">     
+                </div>
+            </div>
+        </article>
     </section>
     `
 }
