20dc7cad16f51c177ad9b2ef48e6fd79d2da6aa0
auroreo <aurore.vernet@live.com>
2024-10-28 20:46:03 +0100

    finished about me page

--- vue/About.js
+++ vue/About.js
@@ -14,18 +14,18 @@ export default {
                     </span>
                 </div>
             </div>
-            <div>
+            <div class="aboutme-infos">
                 <ul>
                     <li>Étudiante en <span class="bold">développement web</span></li>
                     <li>En formation à <span class="bold">La Manu</span>, à Amiens</li>
                     <li>25 ans</li>
                 </ul>
                 <div>
-                    <img src="">
-                    <img src="" class="icon">     
-                    <img src="" class="icon">     
-                    <img src="" class="icon">     
-                    <img src="" class="icon">     
+                    <img src="./img/icons/heart.svg">
+                    <img src="./img/icons/handball.svg" class="icon">     
+                    <img src="./img/icons/games.svg" class="icon">     
+                    <img src="./img/icons/camera.svg" class="icon">     
+                    <img src="./img/icons/moto.svg" class="icon">     
                 </div>
             </div>
         </article>
